615bf62222908217b1c90c352a9a6935b796a43b
iqbal <yya@gmail.com>
2024-05-24 15:57:21 +0700

    active bar

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -1,9 +1,4 @@
 <div class="iq-sidebar">
-    <style>
-        .iq-sidebar {
-            width: 270px;
-        }
-    </style>
 
     <div class="iq-sidebar-logo d-flex justify-content-between">
         <a href="{{ route('home') }}">
@@ -21,10 +16,10 @@
     <div id="sidebar-scrollbar">
         <nav class="iq-sidebar-menu">
             <ul id="iq-sidebar-toggle" class="iq-menu">
-                <li><a href="{{ route('dashboard') }}" class="iq-waves-effect" data-title="Dashboard"><i class="ri-home-4-line"></i><span>Dashboard</span></a></li>
-                <li><a href="{{ route('sensor') }}" class="iq-waves-effect" data-title="Sensor"><i class="ri-list-check"></i><span>Sensor</span></a></li>
-                <li><a href="{{ route('led') }}" class="iq-waves-effect" data-title="Led Control"><i class="ri-lightbulb-line"></i><span>Led Control</span></a></li>
-                <li><a href="{{ route('user') }}" class="iq-waves-effect" data-title="User"><i class="ri-user-line"></i><span>User</span></a></li>
+                <li class="{{ Request::is('dashboard') ? 'active' : '' }}"><a href="{{ route('dashboard') }}" class="iq-waves-effect" data-title="Dashboard"><i class="ri-home-4-line"></i><span>Dashboard</span></a></li>
+                <li class="{{ Request::is('sensor') ? 'active' : '' }}"><a href="{{ route('sensor') }}" class="iq-waves-effect" data-title="Sensor"><i class="ri-list-check"></i><span>Sensor</span></a></li>
+                <li class="{{ Request::is('led*') ? 'active' : '' }}"><a href="{{ route('led') }}" class="iq-waves-effect" data-title="Led Control"><i class="ri-lightbulb-line"></i><span>Led Control</span></a></li>
+                <li class="{{ Request::is('user') ? 'active' : '' }}"><a href="{{ route('user') }}" class="iq-waves-effect" data-title="User"><i class="ri-user-line"></i><span>User</span></a></li>
             </ul>
         </nav>
         <div class="p-3"></div>
