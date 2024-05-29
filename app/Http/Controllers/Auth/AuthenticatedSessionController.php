8741fe706841f813379d9a70caa1b86057df3035
iqbal <yya@gmail.com>
2024-05-29 20:35:03 +0700

    Frontend part 1

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('devices', absolute: false));
+        return redirect()->intended(route('sensor.index', absolute: false));
     }
 
     /**
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('devices', absolute: false));
+        return redirect(route('sensor.index', absolute: false));
     }
 }
