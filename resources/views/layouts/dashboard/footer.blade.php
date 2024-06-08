e5cc54fc88ae7f112ce67ee9036d741933ccb3f4
iqbal <yya@gmail.com>
2024-06-08 11:28:03 +0700

    finishing

--- resources/views/layouts/dashboard/footer.blade.php
+++ resources/views/layouts/dashboard/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-white iq-footer">
     <div class="container-fluid">
        <div class="row">
-          <div class="text-right col-lg-6">
+          <div class="col-lg-6">
              Copyright 2024 <a href="#">Qbal</a> All Rights Reserved.
           </div>
        </div>
